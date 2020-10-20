21a392cf5640362de765ce7c75a43fae3b644175
Tej4401 <albert@localhost.localdomain>
2020-10-21 00:23:08 +0530

    removing atlas credentials1

--- iwp_project/routes/upload_routes.js
+++ iwp_project/routes/upload_routes.js
@@ -5,11 +5,6 @@ const path = require('path');
 var User = require('../models/user.js');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-var MONGO_URI = "mongodb+srv://tej:tpa4401@first-bvv78.gcp.mongodb.net/school?retryWrites=true&w=majority";
-// mongoose
-//   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('DATABASE connected Properly!'))
-//   .catch((err) => console.log('Error is ', err.message));
 
 const router = express.Router();
 
